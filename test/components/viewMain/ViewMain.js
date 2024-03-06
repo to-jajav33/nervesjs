fb85863de4c880c8b4a771557ea5951e303ab742
Javier Mendoza <jmendoza@mindresearch.org>
2024-03-06 08:40:55 -0800

    Changing labels

--- test/components/viewMain/ViewMain.js
+++ test/components/viewMain/ViewMain.js
@@ -3,8 +3,32 @@ import { NervioComponent } from "../../NervioComponent.js";
 export class ViewMain extends NervioComponent {
 	constructor() {
 		super();
+		
+		this._handleConnected = this._handleConnected.bind(this);
+		
+		this.data = {
+			username: '',
+			session: crypto.randomUUID(),
+			drawings: {},
+		};
+
+		this._currentLabelIndex = 0;
+		this._labels = ['car', 'fish', 'house', 'tree', 'bicycle', 'guitar', 'pencil', 'clock'];
+
 
 		NervioComponent.import('../sketchPad/SketchPad.js', this.getFileUrl());
+
+		this.addEventListener('connected', this._handleConnected);
+	}
+
+	_handleConnected() {
+		this.refs.refLabel[0].textContent = this._labels[this._currentLabelIndex];
+	
+		this.refs.refNextButton[0].addEventListener('click', () => {
+			this._currentLabelIndex++;
+
+			this.refs.refLabel[0].textContent = this._labels[this._currentLabelIndex];
+		})
 	}
 }
 
